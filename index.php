09f687fcc2f01a648f9fb00c73981158b2702310
finlay54 <finlayjohnston60@gmail.com>
2015-11-17 23:51:00 +0000

    hello world

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<p>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Finlay
+ * Date: 17/11/2015
+ * Time: 23:44
+ */
+
+echo "Hello world";
+echo "hello," . " " . "world!" . "!";
+echo 5*7;
+?>
+</p>
+</body>
+</html>
