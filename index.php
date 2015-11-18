656c42b8eb09ffce9fc6d2f1ae545cd774ea36f5
finlay54 <finlayjohnston60@gmail.com>
2015-11-18 00:03:19 +0000

    specs, mugs and sausage rolls 2nd excercise

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  * Time: 23:44
  */
 
-$myname = "Frodo Baggins";
+/*$myname = "Frodo Baggins";
 $myage = 111;
 echo "My name is" . $myname . " and I am " . $myage;
 
@@ -20,7 +20,23 @@ if($name == "Simon"){
     print "I know you!";
 }else{
     print "who are you?";
+}*/
+$wantedgood = "mugs";
+switch($wantedgood){
+    case "specs":
+        echo "you must be 8 years old to buy specs";
+        break;
+    case "mugs":
+        echo "you have to be 18 to buy mugs";
+        break;
+    case "sausage rolls":
+        echo "you must be 6 months old to buy sausage rolls";
+        break;
+    default:
+        echo "top kek";
+
 }
+
 ?>
 </p>
 </body>
